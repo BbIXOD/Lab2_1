3706886d6920c8f031f8aa210daf815548b3c5f5
BbIXOD <108470719+BbIXOD@users.noreply.github.com>
2023-02-13 16:02:05 +0200

    Update check

--- main.c
+++ main.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
+#include <math.h>
 #include "Rec1.h"
 #include "Rec2.h"
 #include "Rec3.h"
-#include "Cycle.h"
 #include "Rec3_2.h"
+#include "Cycle.h"
 
 float check(float foo, float res) {
-    printf(res == foo ? "Correct!   " : "Invalid!  ");
+    float delta = res - foo;
+    if (delta < 0) delta *= -1;
+    if (res < 0) res *= -1;
+    const float percent = delta / res * 100;
+    if (res == 0) printf("delta = %.1f  ", delta);
+    else {
+        printf(percent <= 20 ? "Correct! " : "Invalid! "); //in school physics 20% it's OK
+        printf("delta = %.1f%%  ", percent);
+    }
     return foo;
 }
 
@@ -19,11 +28,12 @@ int main() {
     printf("Input x:");
     scanf("%f", &x);
 
-    trueRes = cycle(x, depth);
-    printf("\"Cyclic\" checking function output: %f\n", trueRes);
+    trueRes = logf(x);
+    printf("Real result is: %f\n", trueRes);
+    printf("\"Cyclic\" checking function output: %f\n", check(cycle(x, depth), trueRes));
     printf("\"There\" recursive function output: %f\n", check(recFirstWay(x, depth), trueRes));
     printf("\"From there\" recursive function output: %f\n", check(recSecondWay(x, depth), trueRes));
-    printf("\"Combined\" recursive function output: %f\n", check(recThirdWay2(x, depth, 0), trueRes));
+    printf("\"Combined\" recursive function output: %f\n", check(recThirdWay(x, depth), trueRes));
 
     return 0;
 }
